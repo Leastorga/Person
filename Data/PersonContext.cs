b531f0fa638df85f266a162f44eb6edb45470bbd
Astorgale <lastorga@websupply.com.br>
2024-11-07 10:42:16 -0300

    Add project files.

---
+++ Data/PersonContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Person.Models;
+
+namespace Person.Data
+{
+    public class PersonContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<PersonModel> People { get; set; }
+
+
+    }
+}
